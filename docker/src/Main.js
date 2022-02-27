cb1eb797a9ead9ce8612e16126c50978c1e654d2
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-02-27 22:59:46 +1100

    cemantic cards loaded

--- docker/src/Main.js
+++ docker/src/Main.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { Component } from "react/cjs/react.production.min";
 // import { Component } from "react/cjs/react.production.min";
 import Title from "./Title";
+import CardExampleGroups from "./CardExampleGroups"
 
 class Main extends Component{
     render(){
         return(
             <div>
                 <Title title="Dockit"/>
+                <CardExampleGroups/>
             </div>
         )
     }
--- docker/src/Title.js
+++ docker/src/Title.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Title (props) {
     return (
-        <h1>{props.title}</h1>
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+        
+        
     )
 }
 
