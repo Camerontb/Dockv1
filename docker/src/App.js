f53931f80b7e2b1aadbce0918d8fe68dd7507932
khannashrey07@gmail.com <khannashrey07@gmail.com>
2022-03-06 13:18:59 +1000

    Fix form on button click

--- docker/src/App.js
+++ docker/src/App.js
@@ -1,47 +1,13 @@
 import React from 'react'
 import {useForm} from 'react-hook-form';
 import { Component } from 'react/cjs/react.production.min';
-import { Button, Checkbox, Form } from 'semantic-ui-react'
-
+import { Button, CardDescription, Checkbox, Form } from 'semantic-ui-react'
+import CardExampleGroups from './CardExampleGroups';
 const App =()=>{
-  const{register,handleSubmit} = useForm();
-
-  const onSubmit =(data) =>{
-    console.log(data)
-  }
 
   return(
-  
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Field>
-        <label>Vendor</label>
-        <input
-          id='vendor' 
-          placeholder='Name of Shop' 
-          name='vendor' 
-          {...register('Vendor')}  
-        />
-      </Form.Field>
-
-      <Form.Field>
-        <label>Total</label>
-        <input  id="total"
-          placeholder='how much was it' 
-          type="number" 
-          name='total' 
-          {...register('Total')}  
-        />
-      </Form.Field>
-
-      <Form.Field>
-        <label>Reciept</label>
-        <input type="file"/>
-      </Form.Field>
-      
-      <Button type='submit' {...register("location")}>Submit</Button>
-    </Form>
+    <CardExampleGroups/>
   )
-
 }
 
 export default App
--- docker/src/CardExampleGroups.js
+++ docker/src/CardExampleGroups.js
@@ -1,34 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Card, Image } from 'semantic-ui-react'
 import App from './App'
+import Form from './UserForm'
+const CardExampleGroups = () => {
+  const [isVisible, setVisible] = useState(false)
 
-const CardExampleGroups = () => (
-  
-  <Card.Group className= "homeBox">
-    <Card >
-      <Card.Content >
-        <Image
-          floated='right'
-          size='mini'
-          src='/images/avatar/large/steve.jpg'
-        />
-        <Card.Header>Lodge a new expanse</Card.Header>
-        
-        <Card.Description>
-          Quickly add a new reciept 
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green' onClick={App}>
-            Add Expanse
-          </Button>
-          
-        </div>
-      </Card.Content>
-    </Card>
-    
-  </Card.Group>
-)
+  const handleClick = () => {
+    setVisible(true)
+  }
+
+  return (
+    <>
+      <Card.Group className="homeBox">
+        <Card>
+          <Card.Content>
+            <Image
+              floated="right"
+              size="mini"
+              src="/images/avatar/large/steve.jpg"
+            />
+            <Card.Header>Lodge a new expanse</Card.Header>
+
+            <Card.Description>Quickly add a new reciept</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className="ui two buttons">
+              <Button basic color="green" onClick={handleClick}>
+                Add Expanse
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
+      </Card.Group>
+      {isVisible && <Form />}
+    </>
+  )
+}
 
 export default CardExampleGroups
