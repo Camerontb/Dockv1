ec085554894229c5cf82659ea3da1df9c788c599
khannashrey07@gmail.com <khannashrey07@gmail.com>
2022-03-12 08:54:55 +1000

    Fix connection error

--- Server/db.js
+++ Server/db.js
@@ -1,23 +1,15 @@
-const mysql = require ('mysql2'); 
-const { createPool } = require('mysql2/promise');
-
+const mysql = require('mysql2')
+const { createPool } = require('mysql2/promise')
 
 //Shrey i've also tried the const connection.query() method before using pool and i still cant write to the DB
 
-
-const pool =mysql.createPool({
-    
-    
-    host:'localhost',
-    user:'postgres',
-    password:'Diesel',
-    database:'cams',
-    port:3500,
-    waitForConnections:true
-
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'postgres',
+  password: 'Diesel',
+  database: 'cams',
+  port: 3500,
+  waitForConnections: true,
 })
 
-module.exports = createPool
-
-
-
+module.exports = pool
