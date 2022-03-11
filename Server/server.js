641465a30c6f44abe27655d50ef713c0313fe517
Cameron <93463703+olliebowe@users.noreply.github.com>
2022-03-11 20:40:07 +1100

    Update for Shrey

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,34 @@
-const express = require ('express')
+const express = require('express')
 const cors = require ('cors')
+const bodyParser = require('body-parser');
+const connection = require('./db');
 
-const app = express ();
 
-app.use(cors())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
 
-app.get('/handleSubmit',(req,res)=>{
-    res.send('List of vendor')
+
+app.post('/EXP', (req,res)=> {
+    console.log(req.body)
+    console.log(connection)
+    const Vendor =req.body.data.Vendor
+    const Total= req.body.data.Total
+    
+    const ADD_QUERY = `INSERT INTO app (vendor) VALUES ('test')`;
+    pool.execute(ADD_QUERY,(err)=>{
+      if(err)console.log(err)
+      else res.send('youve got this')
+      res.send('You can add your dockets here')
+
+    })
+
+    
 })
 
-app.listen(4000, ()=>{
-    console.log('running on port 4000')
+app.listen(4000,()=>{
+  console.log('Running on port 4000')
+
 })
+
 
--- docker/src/UserForm.js
+++ docker/src/UserForm.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Component } from 'react/cjs/react.production.min'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios'
 export default function UserForm() {
   const { register, handleSubmit } = useForm()
   const onSubmit = (data) => {
     console.log(data)
+    axios.post('http://localhost:4000/exp',{
+      data
+      
+
+    })
+    
+    
   }
   return (
     <div>
