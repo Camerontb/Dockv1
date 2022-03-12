93ca1280e5f3244d746f9b7c3d340f834cd955e2
khannashrey07@gmail.com <khannashrey07@gmail.com>
2022-03-13 07:46:12 +1000

    Fix connection error

--- Server/db.js
+++ Server/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 const { createPool } = require('mysql2/promise')
 
 //Shrey i've also tried the const connection.query() method before using pool and i still cant write to the DB
-
+//Can you please check your port no. If its correct. On my system its connecting with port no . 8889
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'postgres',
--- Server/server.js
+++ Server/server.js
@@ -1,34 +1,26 @@
 const express = require('express')
-const cors = require ('cors')
-const bodyParser = require('body-parser');
-const connection = require('./db');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const pool = require('./db')
 
-
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/EXP', (req, res) => {
+  console.log(req.body)
+  console.log(pool)
+  const Vendor = req.body.data.Vendor
+  const Total = req.body.data.Total
 
-app.post('/EXP', (req,res)=> {
-    console.log(req.body)
-    console.log(connection)
-    const Vendor =req.body.data.Vendor
-    const Total= req.body.data.Total
-    
-    const ADD_QUERY = `INSERT INTO app (vendor) VALUES ('test')`;
-    pool.execute(ADD_QUERY,(err)=>{
-      if(err)console.log(err)
-      else res.send('youve got this')
-      res.send('You can add your dockets here')
-
-    })
-
-    
+  const ADD_QUERY = `INSERT INTO app (vendor) VALUES ('test')`
+  pool.execute(ADD_QUERY, (err) => {
+    if (err) console.log(err)
+    else res.send('youve got this')
+    res.send('You can add your dockets here')
+  })
 })
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
   console.log('Running on port 4000')
-
 })
-
-
